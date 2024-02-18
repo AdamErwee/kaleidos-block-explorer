6f3e9b2ee443b10ffbb5ebda7c670bd05abe8c49
AdamErwee <adam@openvantage.co.za>
2024-02-18 13:59:59 +0200

    feat: chain nested layouts

---
+++ src/app/block-explorer/layout.tsx
@@ -0,0 +1,16 @@
+const Layout = ({
+    children,
+}: {
+    children: React.ReactNode
+}) => {
+    // Chain List sidebar left
+    // Search header
+    // Table header
+    return (
+        <div>
+            {children}
+        </div>
+    )
+}
+
+export default Layout;
--- src/app/block-explorer/page.tsx
+++
@@ -1,6 +0,0 @@
-
-const BlockExplorer: React.FC = () => {
-  return <h1>Block Explorer</h1>
-}
-
-export default BlockExplorer;
