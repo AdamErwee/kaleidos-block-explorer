687cc48765d93d011f0f6d422912bd437104218b
AdamErwee <adam@openvantage.co.za>
2024-02-18 16:20:54 +0200

    chore: cleanup

--- src/app/Providers.tsx
+++
@@ -1,9 +0,0 @@
-"use client";
-
-import StyledComponentsRegistry from "../lib/registry";
-
-const Providers = (props: React.PropsWithChildren) => {
-  return <StyledComponentsRegistry>{props.children}</StyledComponentsRegistry>;
-};
-
-export default Providers;
