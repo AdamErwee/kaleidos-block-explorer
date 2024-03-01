9da465e065f06d7b3faa3ec558f63da178afd575
AdamErwee <adam@openvantage.co.za>
2024-03-01 17:35:19 +0200

    chore: added meta data

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,13 @@
 import Providers from "./providers";
 
+import type { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Adam's Block Explorer",
+  description:
+    "A humble Block Explorer for Bitcoin, Bitcoin Cash and Ethereum!",
+};
+
 const RootLayout = (props: React.PropsWithChildren) => {
   return (
     <html>
